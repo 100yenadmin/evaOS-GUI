7870a034bb2ceff73132c4a07dd56d5d52f025eb
Eva <eva@100yen.org>
2026-06-14 13:25:44 +0700

    feat(settings): import model selector UX polish

--- packages/desktop/src/renderer/services/i18n/i18n-keys.d.ts
+++ packages/desktop/src/renderer/services/i18n/i18n-keys.d.ts
@@ -1674,6 +1674,7 @@ export type I18nKey =
   | 'settings.model'
   | 'settings.modelCount'
   | 'settings.modelName'
+  | 'settings.modelNotFoundAfterUrlChange'
   | 'settings.modelPlatform'
   | 'settings.modelProtocol'
   | 'settings.modelProtocolTip'
